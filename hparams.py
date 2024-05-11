0e0b4117939bfa2608292d34eb7515057ac7991d
psvnbhavanishankar001 <psvnbhavanishankar001@gmail.com>
2024-05-11 20:03:39 +0530

    changes

--- hparams.py
+++ hparams.py
@@ -1,14 +1,58 @@
 import tensorflow as tf
-
+from collections import namedtuple
 
 # Default hyperparameters:
-hparams = tf.contrib.training.HParams(
-    # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
-    # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
-    cleaners='english_cleaners',
-    use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
+# hparams = tf.contrib.training.HParams(
+#     # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
+#     # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
+#     cleaners='english_cleaners',
+#     use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
 
-    # Audio:
+#     # Audio:
+#     num_mels=80,
+#     num_freq=1025,
+#     sample_rate=20000,
+#     frame_length_ms=50,
+#     frame_shift_ms=12.5,
+#     preemphasis=0.97,
+#     min_level_db=-100,
+#     ref_level_db=20,
+
+#     # Model:
+#     # TODO: add more configurable hparams
+#     outputs_per_step=5,
+#     padding_idx=None,
+#     use_memory_mask=False,
+
+#     # Data loader
+#     pin_memory=True,
+#     num_workers=2,
+
+#     # Training:
+#     batch_size=32,
+#     adam_beta1=0.9,
+#     adam_beta2=0.999,
+#     initial_learning_rate=0.002,
+#     decay_learning_rate=True,
+#     nepochs=1000,
+#     weight_decay=0.0,
+#     clip_thresh=1.0,
+
+#     # Save
+#     checkpoint_interval=5000,
+
+#     # Eval:
+#     max_iters=200,
+#     griffin_lim_iters=60,
+#     power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
+# )
+
+def get_hparams(**kwargs):
+    return namedtuple('GenericDict', kwargs.keys())(**kwargs)
+
+hparams = get_hparams( 
+    cleaners='english_cleaners',
+    use_cmudict=False, 
     num_mels=80,
     num_freq=1025,
     sample_rate=20000,
@@ -17,18 +61,11 @@ hparams = tf.contrib.training.HParams(
     preemphasis=0.97,
     min_level_db=-100,
     ref_level_db=20,
-
-    # Model:
-    # TODO: add more configurable hparams
     outputs_per_step=5,
     padding_idx=None,
     use_memory_mask=False,
-
-    # Data loader
     pin_memory=True,
     num_workers=2,
-
-    # Training:
     batch_size=32,
     adam_beta1=0.9,
     adam_beta2=0.999,
@@ -37,15 +74,10 @@ hparams = tf.contrib.training.HParams(
     nepochs=1000,
     weight_decay=0.0,
     clip_thresh=1.0,
-
-    # Save
     checkpoint_interval=5000,
-
-    # Eval:
     max_iters=200,
     griffin_lim_iters=60,
-    power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
-)
+    power=1.5,  )
 
 
 def hparams_debug_string():
